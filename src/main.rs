cf89f546d26ad6ac407c7f7a73440d2a9bb05a61
V <aron.gjendem@gmail.com>
2023-03-15 14:49:17 +0100

    whisky 4

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use data::TodoState;
 use druid::{WindowDesc, AppLauncher};
+use im::Vector;
+use saver::read_stored;
 use ui::ui_builder;
 
 mod ui;
@@ -9,7 +12,14 @@ fn main() {
     let main_window = WindowDesc::new(ui_builder())
         .title("my app thingy")
         .window_size((400., 400.));
+
+    let stored = read_stored();
+    let default_state = TodoState {
+        todos: Vector::from(stored.tasks),
+        ..Default::default()
+    };
+    
     AppLauncher::with_window(main_window)
-        .launch(data::TodoState::default())
+        .launch(default_state)
         .expect("Failed to start")
 }
--- src/saver.rs
+++ src/saver.rs
@@ -1,8 +1,8 @@
-use std::path::Path;
+use std::{path::Path, fs};
 
 use directories::BaseDirs;
 use druid::Widget;
-use serde::{Serialize, Deserialize};
+use serde::{Serialize, Deserialize,};
 
 use crate::data::{TodoState, TodoItem};
 
@@ -23,6 +23,7 @@ impl Widget<TodoState> for Saver {
                 let config = format!("{}/{}", base_dirs.config_dir().to_str().unwrap(), "my app thingy.json");
                 let config_path = Path::new(&config);
                 let tasks = TaskData {tasks: data.todos.clone().into_iter().collect() };
+                fs::write(config_path, serde_json::to_string(&tasks).unwrap()).expect("Config path does not fully exist")
             }
         }
     }
@@ -38,6 +39,26 @@ impl Widget<TodoState> for Saver {
 } 
 
 #[derive(Serialize, Deserialize)]
-struct TaskData {
+pub struct TaskData {
     pub tasks: Vec<TodoItem>,
+}
+
+pub fn read_stored() -> TaskData {
+    if let Some(base_dirs) = BaseDirs::new() {
+        let config = format!("{}/{}", base_dirs.config_dir().to_str().unwrap(), "MyTodo.json");
+        let config_path = Path::new(&config);
+        let data = match fs::read_to_string(config_path) {
+            Ok(a) => a,
+            Err(_) => return TaskData {tasks: Vec::new()},
+        };
+        match serde_json::from_str(&data) {
+            Ok(a) => a,
+            Err(e) => {
+                eprintln!("The save data is corrupted or no longer in the format it should be in\nError {}", e);
+                return TaskData { tasks: Vec::new() };
+            },
+        }
+    } else {
+        return TaskData { tasks: Vec::new() };
+    }
 }
